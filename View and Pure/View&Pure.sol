2c4ae6ee24465ef423665ed287c5359fbf7b4efa
Rohit Shukla <rohitshukla556@gmail.com>
2018-01-13 13:46:09 +0530

    Create View&Pure.sol

---
+++ View and Pure/View&Pure.sol
@@ -0,0 +1,20 @@
+//view function
+//constant is an alias to view.
+
+pragma solidity ^0.4.18;
+
+contract C {
+    function f(uint a, uint b) public view returns (uint) {
+        return a * (b + 42) + now;
+    }
+}
+
+//pure function
+//Functions can be declared pure in which case they promise not to read from or modify the state.
+pragma solidity ^0.4.18;
+
+contract C {
+    function f(uint a, uint b) public pure returns (uint) {
+        return a * (b + 42);
+    }
+}
