3e47d964132ce15634e529439b151be47e1cf553
rohitshukla11 <rohitshukla556@gmail.com>
2018-01-19 16:25:16 +0530

    first commit

---
+++ 25 - Complete Example - Ticketing System Smart Contract/TicketSystem.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.4.18;
+
+/** @title Ticket System.*/
+contract TicketSystem{
+    address private owner;
+    uint public totalTickets;
+    uint public totalTicketsSold;
+    uint public ticketPrice;
+    uint private totalRevenue;
+    string public standard = "TICKETINGSYSTEM-0.1";
+    mapping(uint => address) public seatsToAttendees;
+    mapping(address => uint) public attendeesToSeats;
+
+    event TicketPurchasedEvent(address indexed _attendee, uint _amount);
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function TicketSystem(uint inTotalTickets, uint inTicketPrice) public{
+        owner = msg.sender;
+        totalRevenue = 0;
+        totalTicketsSold = 0;
+        totalTickets = inTotalTickets;
+        ticketPrice = inTicketPrice;
+    }
+
+    /** @dev Returns the number of unconsumed tickets per user
+    *
+    */
+    function getUserTicketCount() public view returns (uint){
+      return attendeesToSeats[msg.sender];
+    }
+
+    /**@dev Purchases tickets for the event
+    * @param amount Number of tickets to purchase.
+    */
+    function purchaseTicket(uint amount) public payable returns (bool){
+        uint transactionTotal = amount * ticketPrice;
+        // verify: (1) tickets left, (2) amount is >0, (3) user has the money
+        if((amount + totalTicketsSold <= totalTickets) && (amount > 0) && (msg.value>transactionTotal)){
+
+        // track the total revenue
+        totalRevenue += transactionTotal;
+        // send it to owner account
+        for (uint i = totalTicketsSold + 1; i < totalTicketsSold + amount + 1; i++) {
+            seatsToAttendees[i] = msg.sender;
+        }
+        attendeesToSeats[msg.sender] += amount;
+        totalTicketsSold += amount;
+        totalTickets -= amount;
+        TicketPurchasedEvent(msg.sender, amount);
+        return true;
+        } else{
+            return false;
+        }
+  }
+
+    function withdrawAll() onlyOwner public payable returns (bool){
+          uint amount = totalRevenue;
+          // Remember to zero the pending refund before
+          // sending to prevent re-entrancy attacks
+          totalRevenue = 0;
+          msg.sender.transfer(amount);
+    }
+}
